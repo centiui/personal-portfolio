ddef85553bfd6919fa6785e28ed4254e6852c707
Centi <98369279+lovecenti@users.noreply.github.com>
2022-10-01 21:12:57 -0400

    Initial Commit
    This is the first upload and version of the site.

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Centi, a professional web development and UI/UX design service.">
+    <meta property="og:image" content="https://i.imgur.com/mshybFI.png">
+    <title>CentiUI | Web Development & UI/UX Design</title>
+
+    <link rel="stylesheet" href="assets/css/loader.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
+</head>
+
+<body>
+    <div id="entiresite">
+        <header>
+            <nav class="navbar navbar-expand-md fixed-top">
+                <div class="container">
+                    <a class="navbar-brand" href="#"><img src="assets/img/CENTI.png" width="50%" alt=""></a>
+                    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbar">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#vouches">Vouches</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#pricing">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#payment">Payment Options</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="hero">
+                <div class="hero-start">
+                    <div class="container hero-container">
+                        <div class="hero-content my-auto text-center">
+                            <h1>Custom websites, designs, and advertisements</h1>
+                            <h4>Get professional services tailored for your business</h4>
+                            <a href="#pricing" class="btn btn-primary herobtn btn-lg mt-2"><span class="herobtnspan">Learn How</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="vouches" id="vouches">
+                <div class="container">
+                    <div class="row d-flex justify-content-center">
+                        <div class="vouches-header">
+                            <h2 style="width: 50%;">Trusted by independent artists and businesses worldwide</h2>
+                        </div>
+                        <div class="col-xl-3">
+                            <div class="card vouch-card">
+                                <div class="card-body">
+                                    <div class="vouch-person d-flex align-items-center">
+                                        <img src="assets/img/packeyes.png" width="16%" height="16%" alt="">
+                                        <p>Packeyes, Inc</p>
+                                    </div>
+                                    <div class="vouch-content-container">
+                                        <div class="vouch-content">
+                                            “Centi crushed it doing my site, went above and beyond what was expected”<br>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3">
+                            <div class="card vouch-card">
+                                <div class="text-warning position-relative"><i class="fa-solid fa-crown fa-2x position-absolute top-0 start-100 translate-middle"></i></div>
+                                <div class="card-body">
+                                    <div class="vouch-person d-flex align-items-center">
+                                        <img src="assets/img/jumex.png" width="16%" height="16%" style="border-radius:50px" alt="">
+                                        <p>JUMEX</p>
+                                    </div>
+                                    <div class="vouch-content-container">
+                                        <div class="vouch-content">
+                                            “Centi always gets digital work done on time. Been with them for two years and don’t plan on leaving. 11/10”
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3">
+                            <div class="card vouch-card">
+                                <div class="card-body">
+                                    <div class="vouch-person d-flex align-items-center">
+                                        <img src="assets/img/heaven.gif" width="16%" height="16%" style="border-radius:50px" alt="">
+                                        <p>Heaven</p>
+                                    </div>
+                                    <div class="vouch-content-container">
+                                        <div class="vouch-content">
+                                            “Centi is the developer of my website, paste.army, quality was excellent and work was quick asf.”
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pricing" id="pricing">
+                <div class="container">
+                    <div class="pricing-content">
+                        <div class="pricing-header">
+                            <h1>Pricing</h1>
+                        </div>
+                        <div class="row mt-4">
+                            <div class="col-xl-4">
+                                <div class="card pricing-card">
+                                    <div class="card-header pricing-card-header">
+                                        <div class="pch-desc">
+                                            <p>1-page landing<br>
+                                            <h3>$199</h3>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="price-header text-center">Website</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> Design Concept</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 1-week delivery</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 3 edits</div>
+                                        <div class="price-header text-center">Pricing</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 50% Upfront</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 50% Upon Finish</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> Unhappy? Refunds <br>negotiable</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4">
+                                <div class="card pricing-card">
+                                    <div class="card-header pricing-card-header">
+                                        <div class="pch-desc">
+                                            <p>Multi-page landing<br>
+                                            <h3>$299</h3>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="price-header text-center">Website</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> Design Concept</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 1-week delivery</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 3 edits</div>
+                                        <div class="price-header text-center">Pricing</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 50% Upfront</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 50% Upon Finish</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> Unhappy? Refunds <br>negotiable</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4">
+                                <div class="card pricing-card">
+                                    <div class="card-header pricing-card-header">
+                                        <div class="pch-desc">
+                                            <p>Need something else?<br>
+                                            <h3>?????</h3>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="price-header text-center">Website</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> Design Concept</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> ? delivery</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> ? edits</div>
+                                        <div class="price-header text-center">Pricing</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 50% Upfront</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> 50% Upon Finish</div>
+                                        <div class="price-perk"><i class="fa-solid fa-arrow-right"></i> Unhappy? Refunds <br>negotiable</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-12">
+                                <div class="pricing-card card mt-4">
+                                    <div class="card-body" style="padding:3.5rem;">
+                                        <div class="pricing-header d-flex justify-content-center">
+                                            <h1 style="width:60%;">Unsure which price bracket your business fits?</h1>
+                                        </div>
+                                        <div class="unsurebtns d-flex justify-content-center">
+                                            <a href="" class="btn btn-primary unsurebtn btn-lg mt-2"><span class="herobtnspan">Learn How</span></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="payment" id="payment">
+                <div class="container payment">
+                    <h1 class="text-white text-center mb-4 payment-header">Methods of Payment</h1>
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-xl-8">
+                            <div class="card payment-card">
+                                <div class="card-body text-center" style="padding: 2rem;">
+                                    <img src="assets/img/Cryptos.png" class="img-fluid mb-4" width="20%" alt="">
+                                    <h3 class="payment-body">We accept major cryptocurrencies such as Bitcoin, Ethereum, and Monero.</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="contact" id="contact">
+                <div class="container">
+                    <div class="row d-flex justify-content-center">
+                        <div class="contact-header text-center mb-4">
+                            <h1>Contact Us</h1>
+                        </div>
+                        <div class="col-12">
+                            <div class="card contact-card">
+                                <div class="card-body contact-body text-center">
+                                    <a href="https://www.instagram.com/centiui/">Contact us on <i class="fa-brands fa-instagram"></i> Instagram!</a><br>
+                                    <a href="https://t.me/neverlisten">Contact us on <i class="fa-brands fa-telegram"></i> Telegram!</a><br>
+                                    <a href="https://discord.gg/NA8ye87gap">Contact us on <i class="fa-brands fa-discord"></i> Discord!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="footer d-flex">
+                <div class="container">
+                    <div class="row justify-content-center text-center">
+                        <div class="col-xl-4">
+                            <img src="assets/img/CENTI.png" alt=""><br><br>
+                            <a href="" class="footera" style="margin-right:5px;"><i class="fa-solid fa-2x fa-house"></i></a>
+                            <a href="https://www.instagram.com/centiui/" class="footera" style="margin-right:5px;"><i class="fa-brands fa-2x fa-instagram"></i></a>
+                            <a href="https://t.me/neverlisten" class="footera" style="margin-right:5px;"><i class="fa-brands fa-2x fa-telegram"></i></a>
+                            <a href="https://discord.gg/NA8ye87gap" class="footera"><i class="fa-brands fa-2x fa-discord"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="loader-wrapper">
+                <span class="loader"><span class="loader-inner"></span></span>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+            <script>
+                $(window).on("load", function() {
+                    $(".loader-wrapper").fadeOut("slow");
+                });
+            </script>
+        </footer>
+    </div>
+</body>
+
+</html>
